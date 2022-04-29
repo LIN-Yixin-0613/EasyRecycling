8936d06ba6371a35c1a82e358744d8b02f1f6707
LIN-Yixin-0613 <92428316+LIN-Yixin-0613@users.noreply.github.com>
2022-04-29 16:15:56 +0800

    Delete ui directory

--- ui/src/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function NotFound() {
-  return <h1>Page Not Found</h1>;
-}
-
-export default NotFound;
--- ui/src/UserContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const UserContext = React.createContext({
-  signedIn: false,
-});
-
-export default UserContext;
