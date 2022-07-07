e37aed9294a2140b51fe052bd7ffba07bb110927
LIN-Yixin-0613 <92428316+LIN-Yixin-0613@users.noreply.github.com>
2022-07-07 15:26:22 +0800

    Update db.js

--- api/db.js
+++ api/db.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 let db;
 
 async function connectToDb() {
- //会读取到issuetracker
+ //在sample.env中 DB_URL=mongodb://localhost/issuetracker 所以一定会读取到issuetracker 如果不存在sample.env才会读取到recycle
   const url = process.env.DB_URL || 'mongodb://localhost/recycle';
   const client = new MongoClient(url, { useNewUrlParser: true });
   await client.connect();
